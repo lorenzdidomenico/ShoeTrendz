26eb6b05aa0f5fecfe6e2701c706ff9c503735fc
Lorenzo Di Domenico <lorenzo.didomenico@edu.unito.it>
2023-09-17 17:31:38 +0200

    Aggiunto logo e nome sito in alto a sinistra

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="nav" class="nav">
+    <div class="brand">
+      <img
+        src="https://www.stickersmurali.com/it/img/as1281-jpg/folder/products-listado-merchanthover/adesivi-murali-logo-nike.jpg"
+      />
+      Nike
+    </div>
     <nav>
       <router-link v-bind:to="{ name: 'shoes-list' }">Scarpe</router-link>
       | Scarpe: {{ numeroScarpe }} | Carrello ({{ carrello }})
@@ -33,19 +39,31 @@ export default {
   align-items: center;
   height: 60px;
 }
-.nav > .brand {
+
+.brand {
+  display: flex;
+  align-items: center;
   font-family: "Montserrat", sans-serif;
   font-weight: 700;
   font-size: 1.5em;
-  color: #39b982;
+  color: #000000;
   text-decoration: none;
 }
+
+.brand img {
+  max-width: 40px;
+  /* Imposta la larghezza massima del logo */
+  margin-right: 10px;
+  /* Aggiunge spazio tra il logo e il nome del sito */
+}
+
 .nav .nav-item {
   box-sizing: border-box;
   margin: 0 5px;
   color: rgba(0, 0, 0, 0.5);
   text-decoration: none;
 }
+
 .nav .nav-item.router-link-exact-active {
   color: #39b982;
   border-bottom: solid 2px #39b982;
