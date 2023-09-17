2ade9b4036bf7ffc1c0a38723f26eba711d5f069
Lorenzo Di Domenico <lorenzo.didomenico@edu.unito.it>
2023-09-17 21:51:41 +0200

    carrello mezzo funzionante

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,31 +1,31 @@
-<template>
+<!--<template>
   <div>
     <h1>Carrello</h1>
     <div v-if="cartItems.length === 0">
       <p>Il carrello è vuoto.</p>
     </div>
-    <!-- Commenta il componente CartCard -->
-    <!--
     <div v-else>
       <div v-for="item in cartItems" :key="item.id">
         <CartCard :item="item"></CartCard>
       </div>
     </div>
-    -->
   </div>
 </template>
+-->
+
+<div v-if="cartItems.length === 0">
+  <p>Il carrello è vuoto.</p>
+</div>
+<div v-else>
+  <!-- Itera sugli elementi nel carrello e visualizzali qui -->
+</div>
 
 <script>
-import CartCard from "@/components/CartCard.vue";
-
 export default {
-  components: {
-    CartCard,
-  },
-  computed: {
-    cartItems: function () {
-      return this.$store.state.scarpeNelCarrello;
-    },
+  data() {
+    return {
+      cartItems: [], // Assicurati che cartItems sia inizializzata come un array vuoto
+    };
   },
 };
 </script>
