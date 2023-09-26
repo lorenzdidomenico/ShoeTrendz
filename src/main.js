8300ffe2bda33c75f1c6cffa381c6a128b470635
Lorenzo Di Domenico <lorenzo.didomenico@edu.unito.it>
2023-09-26 12:31:45 +0200

    Carosello si vede solo la prima slide

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "jquery";
+import "popper.js";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 
 createApp(App).use(store).use(router).mount("#app");
