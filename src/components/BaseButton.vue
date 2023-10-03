bdc6678860e8fd3ecfbd30d9d65ff78563347aaa
Lorenzo Di Domenico <lorenzo.didomenico@edu.unito.it>
2023-10-03 20:16:05 +0200

    Cambio Stile Pulsanti

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button @click="aggiungiAlCarrello" :disabled="shoe.inventory === 0">
+    <button @click="aggiungiAlCarrello" :disabled="shoe.inventory === 0" class="custom-add-to-cart-button">
       <slot></slot>
     </button>
   </div>
@@ -22,3 +22,42 @@ export default {
   },
 };
 </script>
+
+<style>
+/* Stile del pulsante quando è abilitato */
+.custom-add-to-cart-button {
+  background-color: #007bff;
+  /* Cambia il colore di sfondo azzurro */
+  color: #fff;
+  /* Cambia il colore del testo a bianco */
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+  border-radius: 10px;
+}
+
+/* Stile del pulsante quando è disabilitato */
+.custom-add-to-cart-button:disabled {
+  background-color: #ccc;
+  /* Cambia il colore di sfondo a grigio quando è disabilitato */
+  color: #666;
+  /* Cambia il colore del testo a grigio scuro quando è disabilitato */
+  cursor: not-allowed;
+  /* Cambia il cursore a "non consentito" quando è disabilitato */
+  border-radius: 10px;
+}
+
+.custom-add-to-cart-button:active {
+  background-color: #0056b3;
+  /* Cambia il colore di sfondo azzurro più scuro quando è premuto */
+  color: #fff;
+  /* Cambia il colore del testo a bianco quando è premuto */
+  border-radius: 10px;
+}
+
+/* Stile del pulsante quando ci passi sopra con il mouse */
+.custom-add-to-cart-button:hover {
+  background-color: #0056b3;
+  /* Cambia il colore di sfondo azzurro più scuro quando ci passi sopra con il mouse */
+}
+</style>
