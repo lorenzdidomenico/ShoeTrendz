6ff130eaaa8cbf7dcba74e1cde4fe416d1469901
Lorenzo Di Domenico <lorenzo.didomenico@edu.unito.it>
2023-10-05 10:13:17 +0200

    Creato nuovo componente: una banda orizzontale che può essere utilizzata in più sezioni per riempire

---
+++ src/components/HorizontalBand.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="fascia-orizzontale">
+        <h2 class="titolo">{{ titolo }}</h2>
+        <h3 class="sottotitolo">{{ sottotitolo }}</h3>
+        <button @click="handleClick" class="pulsante">{{ testoPulsante }}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        titolo: {
+            type: String,
+            required: true,
+        },
+        sottotitolo: {
+            type: String,
+            required: true,
+        },
+        testoPulsante: {
+            type: String,
+            default: 'Clicca Qui', // Testo predefinito per il pulsante
+        },
+    },
+    methods: {
+        handleClick() {
+            // Logica da eseguire quando il pulsante viene cliccato
+            console.log('Pulsante cliccato!');
+        },
+    },
+};
+</script>
+
+<style scoped>
+.fascia-orizzontale {
+    background-color: #e77b08;
+    /* Colore di sfondo della fascia orizzontale (blu in questo caso) */
+    color: #fff;
+    /* Colore del testo (bianco in questo caso) */
+    padding: 20px;
+    /* Spaziatura interna della fascia orizzontale */
+    text-align: center;
+}
+
+.titolo {
+    font-weight: bold;
+    font-size: 40px;
+    margin-bottom: 10px;
+}
+
+.sottotitolo {
+
+    font-size: 20px;
+    margin-bottom: 10px;
+}
+
+.pulsante {
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+    background-color: #ffffff;
+    /* Colore di sfondo del pulsante (verde in questo caso) */
+    color: #000000;
+    /* Colore del testo del pulsante (bianco in questo caso) */
+    border: none;
+    /* Rimuovi il bordo del pulsante */
+    border-radius: 20px;
+    /* Bordo arrotondato del pulsante */
+}
+</style>
--- src/views/indexView.vue
+++ src/views/indexView.vue
@@ -4,16 +4,24 @@
 
     <!-- Includi il componente carosello qui -->
     <Carousel />
+
+    <!-- Aggiungi FasciaOrizzontale nella tua vista -->
+    <HorizontalBand titolo="CREA IL TUO STILE CON SHOETRENDZ:" sottotitolo="dove la moda incontra la comodità!"
+      :testoPulsante="'Acquista'" />
+
   </div>
 </template>
 
 <script>
 import Carousel from "@/components/CarouselCard.vue"; // Assicurati che il percorso sia corretto
+import HorizontalBand from "@/components/HorizontalBand.vue"; // Assicurati che il percorso sia corretto
 
 export default {
   components: {
     Carousel, // Utilizza lo stesso nome del componente importato
-  },
+    HorizontalBand,
+  }
+
 };
 </script>
 
