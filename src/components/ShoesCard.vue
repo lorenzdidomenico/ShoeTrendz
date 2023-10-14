04541958d715f885f6e6aebb2e05f7eed4d59f6a
Luca <luca.gardenghi@@edu.unito.it.com>
2023-10-14 12:04:15 +0200

    Tolti commenti ShoesCard

--- src/components/ShoesCard.vue
+++ src/components/ShoesCard.vue
@@ -2,16 +2,22 @@
   <div>
     <div class="shoe-card -shadow">
       <router-link class="shoe-link" v-bind:to="{ name: 'shoes-details', params: { id: shoe.id } }">
+
         <img v-bind:src="shoe.image_url" alt="Immagine delle scarpe" class="img-fluid" />
+
         <h4 class="title">{{ shoe.product }}</h4>
+
+        <!--Verifica dei pezzi disponibili e personalizzazione messaggio-->
         <p v-if="shoe.inventory >= 10">Disponibile</p>
         <p v-else-if="shoe.inventory < 10 && shoe.inventory >= 1">
           In esaurimento
         </p>
         <p v-else style="color: red">Non disponibile</p>
         <p v-show="shoe.onSale">In vendita!</p>
+
         <span class="shoe-price">{{ shoe.price }} euro</span>
       </router-link>
+
       <BaseButton :shoe="shoe">Compra ora a {{ shoe.price }} euro</BaseButton>
     </div>
   </div>
@@ -58,7 +64,6 @@ export default {
   height: auto;
 }
 
-/* Stile per dispositivi mobili */
 @media (max-width: 768px) {
   .shoe-card {
     padding: 10px;
@@ -67,31 +72,13 @@ export default {
   .shoe-card .title {
     font-size: 18px;
   }
-
-  /* Aggiungi altre modifiche necessarie per dispositivi mobili */
 }
 
 @media (min-width: 768px) and (max-width: 1024px) {
   .shoe-card {
     padding: 30px;
     width: 100%;
-    /* Due card per riga su tablet */
     box-sizing: border-box;
-    /* Assicura che il padding non aumenti la larghezza effettiva */
   }
-
-  /* Regola la larghezza delle card in base alle dimensioni del tablet */
-
-
-  /* Altre regole per ottimizzare il layout su tablet */
 }
-
-/* Stile per tablet */
-/* Stile per tablet */
-
-/* Regola la larghezza delle card in base alle dimensioni del tablet */
-
-
-
-/* Aggiungi stili per desktop o altre dimensioni dello schermo */
 </style>
