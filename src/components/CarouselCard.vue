945d80e5b961e92370622cb558c387003770aaf4
Luca <luca.gardenghi@@edu.unito.it.com>
2023-10-14 11:51:28 +0200

    Tolti commenti e frecce a CarouselCard

--- src/components/CarouselCard.vue
+++ src/components/CarouselCard.vue
@@ -18,21 +18,16 @@
             </div>
             <!-- Altre immagini del carousel -->
           </div>
+
+          <div class="carouselIndicators">
           <button type="button" data-bs-target="#mioCarousel" data-bs-slide-to="0" class="active" aria-current="true"
             aria-label="Slide 1"></button>
           <button type="button" data-bs-target="#mioCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
           <button type="button" data-bs-target="#mioCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-          <!-- Indicatori del carousel -->
+        </div>
         </div>
 
-        <button class="carousel-control-prev" type="button" data-bs-target="#mioCarousel" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#mioCarousel" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
+        
       </div>
     </div>
 
@@ -62,53 +57,20 @@ export default {
   padding-top: 20px;
 }
 
-.rosso {
-  background-color: red;
-}
-
-.verde {
-  background-color: green;
-}
-
 .carousel-item img {
   margin: 0;
-  /* Imposta il margin a zero per l'immagine */
   width: 100%;
-  /* Assicura che l'immagine copra completamente lo spazio disponibile in larghezza */
   object-fit: cover;
-  /* Ridimensiona l'immagine in modo da coprire completamente il contenitore */
 }
-
 
 #mioCarousel {
   width: 100%;
-  /* Imposta la larghezza al 100% */
   margin: 0;
-
 }
 
-.carousel-control-prev,
-.carousel-control-next {
-  position: absolute;
-  top: 50%;
-  /* Posiziona le frecce verticalmente al centro rispetto al carosello genitore */
-  transform: translateY(-50%);
-  /* Corregge la posizione verticale per centrare le frecce */
-  width: 10%;
-  /* Imposta la larghezza desiderata delle frecce */
-  height: 100%;
-  /* Imposta l'altezza al 100% per coprire completamente l'altezza del carosello */
-  z-index: 1;
-  /* Imposta un valore di z-index superiore rispetto al carosello */
-}
-
-.carousel-control-prev {
-  left: 0;
-  /* Posiziona la freccia sinistra al bordo sinistro del carosello */
-}
-
-.carousel-control-next {
-  right: 0;
-  /* Posiziona la freccia destra al bordo destro del carosello */
+.carouselIndicators{
+  display: flex;
+  justify-content: center;
+  gap: 10px;
 }
 </style>
