c0734b3f275838aedd4abbca15f568db61731ec4
Luca <luca.gardenghi@@edu.unito.it.com>
2023-10-14 11:41:07 +0200

    Tolti commenti da BaseButton

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -11,25 +11,22 @@ export default {
   methods: {
     aggiungiAlCarrello() {
       if (this.shoe.inventory > 0) {
-        // Chiama la mutazione per aggiungere la scarpa al carrello
+        // Chiamate le mutazione per aggiungere la scarpa al carrello
         this.$store.commit("AGGIUNGI_CARRELLO", this.shoe);
         this.$store.commit("AGGIORNA_CONTEGGIO_CARRELLO");
       }
     },
   },
   props: {
-    shoe: Object, // Ricevi la scarpa come prop dal componente padre
+    shoe: Object, // Si riceve la scarpa come prop dal componente padre
   },
 };
 </script>
 
 <style>
-/* Stile del pulsante quando è abilitato */
 .custom-add-to-cart-button {
   background-color: #007bff;
-  /* Cambia il colore di sfondo azzurro */
   color: #fff;
-  /* Cambia il colore del testo a bianco */
   border: none;
   padding: 10px 20px;
   cursor: pointer;
@@ -39,25 +36,19 @@ export default {
 /* Stile del pulsante quando è disabilitato */
 .custom-add-to-cart-button:disabled {
   background-color: #ccc;
-  /* Cambia il colore di sfondo a grigio quando è disabilitato */
   color: #666;
-  /* Cambia il colore del testo a grigio scuro quando è disabilitato */
   cursor: not-allowed;
-  /* Cambia il cursore a "non consentito" quando è disabilitato */
   border-radius: 10px;
 }
 
 .custom-add-to-cart-button:active {
   background-color: #0056b3;
-  /* Cambia il colore di sfondo azzurro più scuro quando è premuto */
   color: #fff;
-  /* Cambia il colore del testo a bianco quando è premuto */
   border-radius: 10px;
 }
 
-/* Stile del pulsante quando ci passi sopra con il mouse */
+/* Stile del pulsante quando ci si passa sopra con il mouse */
 .custom-add-to-cart-button:hover {
   background-color: #0056b3;
-  /* Cambia il colore di sfondo azzurro più scuro quando ci passi sopra con il mouse */
 }
 </style>
